f0bc7a39a8bf529f3d417ba261603d14f5a89a7b
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-07-13 22:48:29 +0700

    Add more favorite books features

--- backend/controllers/favoriteBookController.js
+++ backend/controllers/favoriteBookController.js
@@ -37,107 +37,33 @@ const createFavorite = asyncHandler(async (req, res) => {
     }
 })
 
-// @desc    Get all subjects
-// @route   GET /api/subjects
+// @desc    Get all favorite books
+// @route   GET /api/favorites
 // @access  Public
-// const getSubjects = asyncHandler(async (req, res) => {
-//     const subjects = await Subject.find({})
-//     res.status(200).json({ 
-//         total: subjects.length, 
-//         totalPoin: generateTotalPoin(subjects),  
-//         subjects })
-// })
+const getFavorites = asyncHandler(async (req, res) => {
+    const favorites = await FavoriteBook.find({})
+    res.status(200).json({ 
+        total: favorites.length, 
+        books: favorites 
+    })
+})
 
-// @desc    Get subjects by category
-// @route   GET /api/subjects/categories/:category
+// @desc    Delete a favorite book
+// @route   DELETE /api/favorites/:id
 // @access  Public
-// const getSubjectsByCategory = asyncHandler(async (req, res) => {
-//     const subjects = await Subject.find({ category: req.params.category.toUpperCase() })
-//     if (subjects) {
-//         res.status(200).json({
-//             total: subjects.length,
-//             totalPoin: generateTotalPoin(subjects),
-//             subjects
-//         })
-//     } else {
-//         res.status(400)
-//         throw new Error('Subject not found')
-//     }
-// })
-
-// @desc    Get subject by id
-// @route   GET /api/subjects/:id
-// @access  Public
-// const getSubject = asyncHandler(async (req, res) => {
-//     const subject = await Subject.findById(req.params.id)
-//     if (subject) {
-//         res.status(200).json(subject)
-//     } else {
-//         res.status(400)
-//         throw new Error('Subject not found')
-//     }
-// })
-
-// @desc    Get subject categories
-// @route   GET /api/subjects/categories
-// @access  Public
-// const getSubjectCategories = asyncHandler(async (req, res) => {
-//     const cateogry = await Subject.aggregate([
-//         { $unwind: "$category" },
-//         {
-//             $group: {
-//                 _id: "$category",
-//                 count: { $sum: 1 },
-//             },
-//         },
-//     ])
-//     // const subject = await Subject.findById(req.params.id)
-//     if (cateogry) {
-//         res.status(200).json(cateogry)
-//     } else {
-//         res.status(400)
-//         throw new Error('Cateogry not found')
-//     }
-// })
-
-// @desc    Ubdate a subject
-// @route   PUT /api/subjects/:id
-// @access  Private/Admin
-// const updateSubject = asyncHandler(async (req, res) => {
-//     const subject = await Subject.findById(req.params.id)
-//     if (subject) {
-//         subject.name = req.body.name || subject.name
-//         subject.category = req.body.category || subject.category
-//         subject.targets = generateTargets(
-//             req.body.totalPages, 
-//             req.body.targets, 
-//             req.body.newTargets,
-//             subject
-//         ) || subject.targets
-        
-//         const updatedSubject = await subject.save()
-//         res.status(200).json(updatedSubject)
-//     } else {
-//         res.status(400)
-//         throw new Error('Subject not found')
-//     }
-// })
-
-// @desc    Delete a subject
-// @route   Delete /api/subjects/:id
-// @access  Private/Admin
-// const deleteSubject = asyncHandler(async (req, res) => {
-//     const subject = await Subject.findById(req.params.id)
-//     if (subject) {
-//         await subject.remove()
-//         res.status(200).json({ id: req.params.id })
-//     } else {
-//         res.status(400)
-//         throw new Error('Subject not found')
-//     }
-// })
-
+const deleteFavorite = asyncHandler(async (req, res) => {
+    const favorite = await FavoriteBook.findById(req.params.id)
+    if (favorite) {
+        await favorite.remove()
+        res.json({ id: req.params.id })
+    } else {
+        res.status(404)
+        throw new Error('Book not found')
+    }
+})
 
 export { 
     createFavorite,
+    getFavorites,
+    deleteFavorite
 }
--- backend/routes/favoriteBookRoutes.js
+++ backend/routes/favoriteBookRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { 
-    createFavorite
+    createFavorite,
+    deleteFavorite,
+    getFavorites
 } from '../controllers/favoriteBookController.js'
 
 const router = express.Router()
 router.route('/')
     .post(createFavorite)
+    .get(getFavorites)
+router.route('/:id')
+    .delete(deleteFavorite)
 
 export default router
