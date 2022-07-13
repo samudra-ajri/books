e66ec8c478a89878f23621d5563477f2784ac0b5
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-07-13 21:13:29 +0700

    Add home route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+
+import homeRoutes from './routes/homeRoute.js'
+
+dotenv.config()
+const app  = express()
+const PORT = process.env.PORT
+const ENV  = process.env.APP_ENV
+
+if (ENV === 'staging' || ENV === 'local') {
+    app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/', homeRoutes)
+
+app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`))
